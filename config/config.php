52ebddcb4c4cd3cd3960e56b72db5582a68ac8f0
Marco Rabozzi <marco.rabozzi@gmail.com>
2017-11-27 11:24:04 +0100

    initial version of the website

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+$_page_info = array(
+    'menu' => array(
+        array(
+            'name' => 'Xilinx PYNQ Hackathon (XPH)',
+            'anchor' => 'http://xph.necst.it/'
+        ),
+        array(
+            'name' => 'Tower of Hanoi (ToH)',
+            'anchor' => 'http://toh.necst.it/'
+        ),
+        array(
+            'name' => 'Wellness and Sports Hackathon',
+            'anchor' => '/wellness-and-sport.php'
+        ),
+        array(
+            'name' => 'Contact',
+            'anchor' => '/contact.php'
+        ),
+        array(
+            'name' => '<img src="/image/fb.png" style="width:24px; margin-top:-3px;">',
+            'anchor' => 'https://www.facebook.com/groups/Hack.NECST'
+        ),
+    )
+);
